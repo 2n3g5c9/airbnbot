f1e39fbe373af3c1ce565ad384ea8ef96d5cc837
Marc Molina <marc.m@outlook.com>
2018-11-10 23:04:44 +0100

    added a simple webhook to test oauth

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 
+require('dotenv').config();
 require('./server.ts');
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,44 @@
 import express from 'express';
+import request from 'request';
 
 const app: express.Application = express();
+const PORT: number = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.listen(PORT, () => {
+  console.log(`Airbnbot listening on port ${PORT}!`);
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.get('/', (req, res) => {
+  res.send(`Ngrok is working! Path Hit: ${req.url}`);
+});
+
+app.get('/oauth', (req, res) => {
+  if (!req.query.code) {
+    res.status(500);
+    res.send({ Error: "Looks like we're not getting code." });
+    console.log("Looks like we're not getting code.");
+  } else {
+    request(
+      {
+        url: 'https://slack.com/api/oauth.access',
+        qs: {
+          code: req.query.code,
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+        },
+        method: 'GET',
+      },
+      (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.json(body);
+        }
+      },
+    );
+  }
+});
+
+app.post('/london', (req, res) => {
+  res.send('Our ngrok tunnel is up and running!');
 });
