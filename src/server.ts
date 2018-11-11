f2b9f2f89af7827a4258187be6e6885e1e97ff2e
Marc Molina <marc.m@outlook.com>
2018-11-11 16:14:37 +0100

    added checkin and checkout params

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import request from 'request'
 
 const app: express.Application = express()
 const PORT: number = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.listen(PORT, () => {
   console.log(`Airbnbot listening on port ${PORT}!`)
@@ -39,14 +42,17 @@ app.get('/oauth', (req, res) => {
   }
 })
 
+const dateRegex: RegExp = /\d{4}-\d{2}-\d{2}/g
+
 app.post('/london', (req, res) => {
+  const dates: string[] = req.body.text.match(dateRegex)
   request.get(
     'https://www.airbnb.com/api/v2/explore_tabs?' +
       'items_per_grid=18' +
       '&timezone_offset=60' +
-      '&client_session_id=ddb9e657-3633-4930-a9ed-fd47c4689a5a' +
-      '&checkin=2018-11-22' +
-      '&checkout=2018-11-25' +
+      `&client_session_id=${process.env.AIRBNB_CLIENT_SESSION_ID}` +
+      `&checkin=${dates[0]}` +
+      `&checkout=${dates[1]}` +
       '&adults=2' +
       '&children=0' +
       '&infants=0' +
@@ -54,7 +60,7 @@ app.post('/london', (req, res) => {
       '&room_types%5B%5D=Entire%20home%2Fapt' +
       '&query=Londres%2C%20Royaume-Uni' +
       '&_intents=p1' +
-      '&key=d306zoyjsyarp7ifhu67rjxn52tv0t20' +
+      `&key=${process.env.AIRBNB_KEY}` +
       '&currency=EUR' +
       '&locale=fr',
     (error, response, body) => {
